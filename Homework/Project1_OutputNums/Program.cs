19036b7882f3432c717850690ef16140b36e1236
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-23 19:07:06 +0300

    Homework. Sec commit

--- Homework/Project1_OutputNums/Program.cs
+++ Homework/Project1_OutputNums/Program.cs
@@ -8,14 +8,39 @@ class Program1
     Пришлось как-то выкручиваться, в чем проблема не до конца понял.*/
 
     Console.WriteLine("Программа выводит числа от 1 до N");
-    System.Console.Write("Задайте N: "); // по условию N надо задать 
-    try
+    System.Console.Write("Задайте N. "); // по условию N надо задать 
+    int N = InputN();
+    int i = 0;
+    Output1toN(N, i);
+
+
+
+
+
+
+    int InputN()
     {
-      int N = Convert.ToInt32(Console.ReadLine());
+      int N = -1;
+      do
+      {
+        System.Console.WriteLine("Введите натуральное число:");
+      try
+      {
+        N = Convert.ToInt32(Console.ReadLine());
+      }
+      catch
+      {
+        System.Console.WriteLine("Ошибка. Введите натуральное число");
+      }
+      } while (N<1);
+      return N;
     }
-    catch
+
+    void Output1toN(int N, int i)
     {
-      System.Console.WriteLine("Введите целое число");
+      System.Console.Write($"{++i}  ");
+      if (i < N)
+        Output1toN(N, i);
     }
   }
 }
